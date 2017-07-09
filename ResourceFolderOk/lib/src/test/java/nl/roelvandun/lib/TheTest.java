0ebd212fb17fd19eb5ad7156e7f505adfe4b115e
Roel van Dun <roelvandun@gmail.com>
2017-07-09 13:55:28 +0200

    ./gradlew lib:test works. Running the test inside Android Studio doesn't :/

--- ResourceFolderOk/lib/src/test/java/nl/roelvandun/lib/TheTest.java
+++ ResourceFolderOk/lib/src/test/java/nl/roelvandun/lib/TheTest.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 
 import java.net.URL;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class TheTest {
 
 	@Test
 	public void name() throws Exception {
-		String filename = "people.json";
+		String filename = "/people.json";
 
 		URL resource1 = getClass().getResource(filename);
 		URL resource2 = getClass().getClassLoader().getResource(filename);
@@ -16,5 +21,10 @@ public class TheTest {
 		URL resource3 = Thread.currentThread().getContextClassLoader().getResource(filename);
 		URL resource4 = ClassLoader.getSystemClassLoader().getResource(filename);
 
+		assertThat(resource1, is(not(nullValue()))); //success
+//		assertThat(resource2, is(not(nullValue()))); //fails
+//		assertThat(resource3, is(not(nullValue()))); //fails
+//		assertThat(resource4, is(not(nullValue()))); //fails
+//		assertThat(resource5, is(not(nullValue()))); //fails
 	}
 }
